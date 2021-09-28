2b2716f625d14aac041a3e2f66278a76e95cb00a
YuraRiabov <uraryabov@gmail.com>
2021-09-28 10:30:07 +0300

    Minor changes in lab3 ОП

--- ОП/Lab3/Lab3 C++/Lab3 C++.cpp
+++ ОП/Lab3/Lab3 C++/Lab3 C++.cpp
@@ -4,28 +4,25 @@ using namespace std;
 
 int main()
 {
-	double number;
-	double previous_value;
-	double current_value;
+	double number; // input number
+	double previous_value; // previos value of root, needed to calculate margin
+	double current_value; // current value of root, output
 
+	// Input of number
 	cout << "Enter a number: ";
 	cin >> number;
 
-	if (number <= 1)
-	{
+	// Asigning current value based on input
+	if (number <= 1){
 		current_value = min(2 * number, 0.95);
-	}
-	else if (number > 1 && number < 25)
-	{
+	}else if (number > 1 && number < 25){
 		current_value = number / 5;
-	}
-	else
-	{
+	}else{
 		current_value = number / 25;
 	}
 
-	do
-	{
+	// Calculating the root
+	do{
 		previous_value = current_value;
 		current_value = 0.8 * previous_value + number / (5 * pow(previous_value, 4));
 	} while (abs(current_value - previous_value) > 1e-4);
