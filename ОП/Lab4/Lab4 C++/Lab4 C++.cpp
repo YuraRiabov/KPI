eb306e528c5d2fed7173decb65091ecf8792903c
YuraRiabov <uraryabov@gmail.com>
2021-09-29 17:31:32 +0300

    Lab4 ОП added

---
+++ ОП/Lab4/Lab4 C++/Lab4 C++.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n; // Number of iterations, input
+	int currentFirstNumber; // First number in formula, defined every iteration
+	int currentSecondNumber; // Second number in formula, defined every iteration
+	int sum; // Sum, output
+
+	cout << "Enter a number: ";
+	cin >> n;
+
+	sum = 0; // At the beginning of cycle sum is 0
+
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 2 == 1)
+		{
+			currentFirstNumber = i;
+			currentSecondNumber = pow(i, 2);
+		}
+		else
+		{
+			currentFirstNumber = i / 2;
+			currentSecondNumber = i + 7;
+		}
+		sum += pow(currentFirstNumber - currentSecondNumber, 2);
+	}
+
+	cout << "The sum is " << sum;
+}
