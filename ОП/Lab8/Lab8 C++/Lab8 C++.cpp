7a8511ae98c3bd626fb6a13d15fa53718cc1974e
YuraRiabov <uraryabov@gmail.com>
2021-11-19 15:32:25 +0200

    Update Lab8 C++.cpp

--- ОП/Lab8/Lab8 C++/Lab8 C++.cpp
+++ ОП/Lab8/Lab8 C++/Lab8 C++.cpp
@@ -4,14 +4,15 @@
 #include <vector>
 using namespace std;
 
-void moveDiagonallyUp(vector<vector<int>> &matrix, int &currentNumber, int &i, int &j, int& size);
-void moveDiagonallyDown(vector<vector<int>> &matrix, int &currentNumber, int &i, int &j, int& size);
-void outputArray(vector<vector<int>> matrix);
+int** CreateMatrix(int rowNumber, int columnNumber);
+void MoveDiagonallyUp(int** matrix, int& currentNumber, int& i, int& j, int size);
+void MoveDiagonallyDown(int** matrix, int& currentNumber, int& i, int& j, int size);
+void OutputMatrix(int** matrix, int rowNumber, int columnNumber);
 
 int main()
 {
 	int matrixSize; // Size of a matrix we wanna create
-	int elementsNumber; // Total number of elements in the matrix
+	int** matrix;
 	int currentNumber; // Current number which is being put in the matrix
 	int i; // Current line of the matrix
 	int j; // Current column of the matrix
@@ -19,11 +20,10 @@ int main()
 	cout << "Enter matrix size: ";
 	cin >> matrixSize;
 
-	vector<vector<int>>matrix(matrixSize, vector<int>(matrixSize)); // Emty matrix of needed size
+	matrix = CreateMatrix(matrixSize, matrixSize);
 
-	elementsNumber = (int)pow(matrixSize, 2);
-	matrix[0][0] = elementsNumber;
-	currentNumber = elementsNumber - 1;
+	matrix[0][0] = (int)pow(matrixSize, 2);
+	currentNumber = matrix[0][0] - 1;
 	i = 0;
 	j = 0;
 
@@ -32,24 +32,24 @@ int main()
 		if (i == 0 && j != matrixSize - 1)
 		{
 			j += 1;
-			moveDiagonallyDown(matrix, currentNumber, i, j, matrixSize);
+			MoveDiagonallyDown(matrix, currentNumber, i, j, matrixSize);
 		}
 		else if (j == 0 && i != matrixSize - 1)
 		{
 			i += 1;
-			moveDiagonallyUp(matrix, currentNumber, i, j, matrixSize);
+			MoveDiagonallyUp(matrix, currentNumber, i, j, matrixSize);
 		}
 		else if (j == matrixSize - 1)
 		{
 			i += 1;
-			moveDiagonallyDown(matrix, currentNumber, i, j, matrixSize);
+			MoveDiagonallyDown(matrix, currentNumber, i, j, matrixSize);
 		}
 		else if (i == matrixSize - 1)
 		{
 			j += 1;
 			if (currentNumber != 1)
 			{
-				moveDiagonallyUp(matrix, currentNumber, i, j, matrixSize);
+				MoveDiagonallyUp(matrix, currentNumber, i, j, matrixSize);
 			}
 			else
 			{
@@ -59,13 +59,25 @@ int main()
 		}
 	}
 
-	outputArray(matrix);
+	OutputMatrix(matrix, matrixSize, matrixSize);
 	cout << "Corner elements of the matrix: " << matrix[0][0] << " " << matrix[matrixSize - 1][0] << " " << matrix[0][matrixSize - 1] << " " << matrix[matrixSize - 1][matrixSize - 1];
 	cout << "\nMaximum corner element of the matrix: " << matrix[0][0];
+
+	system("pause");
+}
+
+int** CreateMatrix(int rowNumber, int columnNumber)
+{
+	int** matrix = new int* [rowNumber];
+	for (int i = 0; i < rowNumber; i++)
+	{
+		matrix[i] = new int[columnNumber];
+	}
+	return matrix;
 }
 
 // Function which fills a diagonal with decreasing values from bottom to top moving to the right
-void moveDiagonallyUp(vector<vector<int>> &matrix, int& currentNumber, int& i, int& j, int& size)
+void MoveDiagonallyUp(int** matrix, int& currentNumber, int& i, int& j, int size)
 {
 	for (i, j; i >= 0 && j <= size - 1; i--, j++)
 	{
@@ -77,7 +89,7 @@ void moveDiagonallyUp(vector<vector<int>> &matrix, int& currentNumber, int& i, i
 }
 
 // Function which fills a diagonal with decreasing values from top to bottom moving to the left
-void moveDiagonallyDown(vector<vector<int>> &matrix, int& currentNumber, int& i, int& j, int& size)
+void MoveDiagonallyDown(int** matrix, int& currentNumber, int& i, int& j, int size)
 {
 	for (i, j; i <= size - 1 && j >= 0; i++, j--)
 	{
@@ -89,11 +101,11 @@ void moveDiagonallyDown(vector<vector<int>> &matrix, int& currentNumber, int& i,
 }
 
 // Function which outputs a matrix
-void outputArray(vector<vector<int>> matrix)
+void OutputMatrix(int** matrix, int rowNumber, int columnNumber)
 {
-	for (int i = 0; i < matrix.size(); i++)
+	for (int i = 0; i < rowNumber; i++)
 	{
-		for (int j = 0; j < matrix.size(); j++)
+		for (int j = 0; j < columnNumber; j++)
 		{
 			cout << setw(4) << matrix[i][j];
 		}
