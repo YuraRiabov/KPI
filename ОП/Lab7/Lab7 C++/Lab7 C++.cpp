42c207b588bef7d3332d5c3ce8fe72dd9f4d2bcf
YuraRiabov <uraryabov@gmail.com>
2021-11-19 13:20:32 +0200

    Redone with dinamic arrays

--- ОП/Lab7/Lab7 C++/Lab7 C++.cpp
+++ ОП/Lab7/Lab7 C++/Lab7 C++.cpp
@@ -2,49 +2,51 @@
 #include <cmath>
 using namespace std;
 
-const int N = 7; // Size of an array
-int arr[N]; // Array
-
-void GenerateArray(int* arr);
-int FindMax(int* array);
-double FindD(int* array, int max);
-void SubtractFromOdd(double d, int* array);
-void OutputArray(int* arr);
+int* GenerateArray(int size);
+int FindMax(int* array, int size);
+double FindD(int* array, int max, int size);
+void SubtractFromOdd(double d, int* array, int size);
+void OutputArray(int* array, int size);
 
 int main()
 {
+	int* array;
+	int size;
 	int max; // Max element of an array
 	int d; // Value every element has to be reduced by
 
-	GenerateArray(arr);
-	cout << "Generated array: ";
-	OutputArray(arr);
-	cout << "\n";
+	cout << "Enter array size: ";
+	cin >> size;
 
-	max = FindMax(arr);
-	d = FindD(arr, max);
-	SubtractFromOdd(d, arr);
+	array = GenerateArray(size);
+	cout << "Generated array: ";
+	OutputArray(array, size);
+
+	max = FindMax(array, size);
+	d = FindD(array, max, size);
+	SubtractFromOdd(d, array, size);
 
 	cout << "New array: ";
-	OutputArray(arr);
+	OutputArray(array, size);
 
 	system("pause");
 }
 
-void GenerateArray(int* array) // Function which generates values for initialized array
+int* GenerateArray(int size) // Function which generates values for initialized array
 {
+	int* array = new int[size];
 	srand(time(NULL));
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < size; i++)
 	{
 		array[i] = rand() % 30 - 15;
 	}
-	
+	return array;
 }
 
-int FindMax(int* array) // Function which returns the maximum value of an array
+int FindMax(int* array, int size) // Function which returns the maximum value of an array
 {
-	int max = array[1];
-	for (int i = 0; i < N; i++)
+	int max = array[0];
+	for (int i = 1; i < size; i++)
 	{
 		if (array[i] > max)
 		{
@@ -54,28 +56,29 @@ int FindMax(int* array) // Function which returns the maximum value of an array
 	return max;
 }
 
-double FindD(int* array, int max)
+double FindD(int* array, int max, int size)
 {
 	double d = 0;
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < size; i++)
 	{
 		d += (int)pow(array[i] - max, 2);
 	}
 	return d;
 }
 
-void SubtractFromOdd(double d, int* array)
+void SubtractFromOdd(double d, int* array, int size)
 {
-	for (int i = 0; i < N; i += 2)
+	for (int i = 0; i < size; i += 2)
 	{
-		arr[i] -= d;
+		array[i] -= d;
 	}
 }
 
-void OutputArray(int* arr) // Function which outputs array elements
+void OutputArray(int* array, int size) // Function which outputs array elements
 {
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < size; i++)
 	{
-		cout << arr[i] << " ";
+		cout << array[i] << " ";
 	}
+	cout << "\n";
 }
--- ОП/Lab8/Lab8 C++/Lab8 C++.cpp
+++ ОП/Lab8/Lab8 C++/Lab8 C++.cpp
@@ -1,17 +1,18 @@
 ﻿#include <iostream>
+#include <iomanip>
 #include <cmath>
 #include <vector>
 using namespace std;
 
-void moveDiagonallyUp(vector<vector<int>> &matrix, int &n, int &currentNum, int &i, int &j, int& size);
-void moveDiagonallyDown(vector<vector<int>> &matrix, int &n, int &currentNum, int &i, int &j, int& size);
+void moveDiagonallyUp(vector<vector<int>> &matrix, int &currentNumber, int &i, int &j, int& size);
+void moveDiagonallyDown(vector<vector<int>> &matrix, int &currentNumber, int &i, int &j, int& size);
 void outputArray(vector<vector<int>> matrix);
 
 int main()
 {
 	int matrixSize; // Size of a matrix we wanna create
-	int n; // Total number of elements in the matrix
-	int currentNum; // Current number which is being put in the matrix
+	int elementsNumber; // Total number of elements in the matrix
+	int currentNumber; // Current number which is being put in the matrix
 	int i; // Current line of the matrix
 	int j; // Current column of the matrix
 
@@ -20,40 +21,40 @@ int main()
 
 	vector<vector<int>>matrix(matrixSize, vector<int>(matrixSize)); // Emty matrix of needed size
 
-	n = (int)pow(matrixSize, 2);
-	matrix[0][0] = n;
-	currentNum = n - 1;
+	elementsNumber = (int)pow(matrixSize, 2);
+	matrix[0][0] = elementsNumber;
+	currentNumber = elementsNumber - 1;
 	i = 0;
 	j = 0;
 
-	while (currentNum > 0)
+	while (currentNumber > 0)
 	{
 		if (i == 0 && j != matrixSize - 1)
 		{
 			j += 1;
-			moveDiagonallyDown(matrix, n, currentNum, i, j, matrixSize);
+			moveDiagonallyDown(matrix, currentNumber, i, j, matrixSize);
 		}
 		else if (j == 0 && i != matrixSize - 1)
 		{
 			i += 1;
-			moveDiagonallyUp(matrix, n, currentNum, i, j, matrixSize);
+			moveDiagonallyUp(matrix, currentNumber, i, j, matrixSize);
 		}
 		else if (j == matrixSize - 1)
 		{
 			i += 1;
-			moveDiagonallyDown(matrix, n, currentNum, i, j, matrixSize);
+			moveDiagonallyDown(matrix, currentNumber, i, j, matrixSize);
 		}
 		else if (i == matrixSize - 1)
 		{
 			j += 1;
-			if (currentNum != 1)
+			if (currentNumber != 1)
 			{
-				moveDiagonallyUp(matrix, n, currentNum, i, j, matrixSize);
+				moveDiagonallyUp(matrix, currentNumber, i, j, matrixSize);
 			}
 			else
 			{
 				matrix[i][j] = 1;
-				currentNum -= 1;
+				currentNumber -= 1;
 			}
 		}
 	}
@@ -64,24 +65,24 @@ int main()
 }
 
 // Function which fills a diagonal with decreasing values from bottom to top moving to the right
-void moveDiagonallyUp(vector<vector<int>> &matrix, int& n, int& currentNum, int& i, int& j, int& size)
+void moveDiagonallyUp(vector<vector<int>> &matrix, int& currentNumber, int& i, int& j, int& size)
 {
 	for (i, j; i >= 0 && j <= size - 1; i--, j++)
 	{
-		matrix[i][j] = currentNum;
-		currentNum -= 1;
+		matrix[i][j] = currentNumber;
+		currentNumber -= 1;
 	}
 	i++;
 	j--;
 }
 
 // Function which fills a diagonal with decreasing values from top to bottom moving to the left
-void moveDiagonallyDown(vector<vector<int>> &matrix, int& n, int& currentNum, int& i, int& j, int& size)
+void moveDiagonallyDown(vector<vector<int>> &matrix, int& currentNumber, int& i, int& j, int& size)
 {
 	for (i, j; i <= size - 1 && j >= 0; i++, j--)
 	{
-		matrix[i][j] = currentNum;
-		currentNum -= 1;
+		matrix[i][j] = currentNumber;
+		currentNumber -= 1;
 	}
 	i--;
 	j++;
@@ -94,7 +95,7 @@ void outputArray(vector<vector<int>> matrix)
 	{
 		for (int j = 0; j < matrix.size(); j++)
 		{
-			cout << matrix[i][j] << " ";
+			cout << setw(4) << matrix[i][j];
 		}
 		cout << "\n";
 	}
