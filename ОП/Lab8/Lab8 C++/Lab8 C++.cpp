3cfccbecf00e8ed16d04bffffa348d27e2473a9a
YuraRiabov <uraryabov@gmail.com>
2021-11-19 15:57:53 +0200

    Update Lab8 C++.cpp

--- ОП/Lab8/Lab8 C++/Lab8 C++.cpp
+++ ОП/Lab8/Lab8 C++/Lab8 C++.cpp
@@ -1,67 +1,29 @@
 ﻿#include <iostream>
 #include <iomanip>
 #include <cmath>
-#include <vector>
 using namespace std;
 
 int** CreateMatrix(int rowNumber, int columnNumber);
-void MoveDiagonallyUp(int** matrix, int& currentNumber, int& i, int& j, int size);
-void MoveDiagonallyDown(int** matrix, int& currentNumber, int& i, int& j, int size);
+void FillByPattern(int** matrix, int matrixSize);
+void MoveDiagonallyUp(int** matrix, int& currentNumber, int& currentRow, int& currentColumn, int size);
+void MoveDiagonallyDown(int** matrix, int& currentNumber, int& currentRow, int& currentColumn, int size);
 void OutputMatrix(int** matrix, int rowNumber, int columnNumber);
+void OutputCorners(int** matrix, int matrixSize);
 
 int main()
 {
-	int matrixSize; // Size of a matrix we wanna create
+	int matrixSize;
 	int** matrix;
-	int currentNumber; // Current number which is being put in the matrix
-	int i; // Current line of the matrix
-	int j; // Current column of the matrix
 
 	cout << "Enter matrix size: ";
 	cin >> matrixSize;
 
 	matrix = CreateMatrix(matrixSize, matrixSize);
-
-	matrix[0][0] = (int)pow(matrixSize, 2);
-	currentNumber = matrix[0][0] - 1;
-	i = 0;
-	j = 0;
-
-	while (currentNumber > 0)
-	{
-		if (i == 0 && j != matrixSize - 1)
-		{
-			j += 1;
-			MoveDiagonallyDown(matrix, currentNumber, i, j, matrixSize);
-		}
-		else if (j == 0 && i != matrixSize - 1)
-		{
-			i += 1;
-			MoveDiagonallyUp(matrix, currentNumber, i, j, matrixSize);
-		}
-		else if (j == matrixSize - 1)
-		{
-			i += 1;
-			MoveDiagonallyDown(matrix, currentNumber, i, j, matrixSize);
-		}
-		else if (i == matrixSize - 1)
-		{
-			j += 1;
-			if (currentNumber != 1)
-			{
-				MoveDiagonallyUp(matrix, currentNumber, i, j, matrixSize);
-			}
-			else
-			{
-				matrix[i][j] = 1;
-				currentNumber -= 1;
-			}
-		}
-	}
-
+	FillByPattern(matrix, matrixSize);
 	OutputMatrix(matrix, matrixSize, matrixSize);
-	cout << "Corner elements of the matrix: " << matrix[0][0] << " " << matrix[matrixSize - 1][0] << " " << matrix[0][matrixSize - 1] << " " << matrix[matrixSize - 1][matrixSize - 1];
-	cout << "\nMaximum corner element of the matrix: " << matrix[0][0];
+	cout << "Corner elements of the matrix: ";
+	OutputCorners(matrix, matrixSize);
+	cout << "\nMaximum corner element of the matrix: " << matrix[0][0] << "\n";
 
 	system("pause");
 }
@@ -76,28 +38,68 @@ int** CreateMatrix(int rowNumber, int columnNumber)
 	return matrix;
 }
 
-// Function which fills a diagonal with decreasing values from bottom to top moving to the right
-void MoveDiagonallyUp(int** matrix, int& currentNumber, int& i, int& j, int size)
+void FillByPattern(int** matrix, int matrixSize)
 {
-	for (i, j; i >= 0 && j <= size - 1; i--, j++)
+	matrix[0][0] = (int)pow(matrixSize, 2);
+	int currentNumber = matrix[0][0] - 1;
+	int currentRow = 0;
+	int currentColumn = 0;
+
+	while (currentNumber > 0)
 	{
-		matrix[i][j] = currentNumber;
+		if (currentRow == 0 && currentColumn != matrixSize - 1)
+		{
+			currentColumn += 1;
+			MoveDiagonallyDown(matrix, currentNumber, currentRow, currentColumn, matrixSize);
+		}
+		else if (currentColumn == 0 && currentRow != matrixSize - 1)
+		{
+			currentRow += 1;
+			MoveDiagonallyUp(matrix, currentNumber, currentRow, currentColumn, matrixSize);
+		}
+		else if (currentColumn == matrixSize - 1)
+		{
+			currentRow += 1;
+			MoveDiagonallyDown(matrix, currentNumber, currentRow, currentColumn, matrixSize);
+		}
+		else if (currentRow == matrixSize - 1)
+		{
+			currentColumn += 1;
+			if (currentNumber != 1)
+			{
+				MoveDiagonallyUp(matrix, currentNumber, currentRow, currentColumn, matrixSize);
+			}
+			else
+			{
+				matrix[currentRow][currentColumn] = 1;
+				currentNumber -= 1;
+			}
+		}
+	}
+}
+
+// Function which fills a diagonal with decreasing values from bottom to top moving to the right
+void MoveDiagonallyUp(int** matrix, int& currentNumber, int& currentRow, int& currentColumn, int size)
+{
+	for (currentRow, currentColumn; currentRow >= 0 && currentColumn <= size - 1; currentRow--, currentColumn++)
+	{
+		matrix[currentRow][currentColumn] = currentNumber;
 		currentNumber -= 1;
 	}
-	i++;
-	j--;
+	currentRow++;
+	currentColumn--;
 }
 
 // Function which fills a diagonal with decreasing values from top to bottom moving to the left
-void MoveDiagonallyDown(int** matrix, int& currentNumber, int& i, int& j, int size)
+void MoveDiagonallyDown(int** matrix, int& currentNumber, int& currentRow, int& currentColumn, int size)
 {
-	for (i, j; i <= size - 1 && j >= 0; i++, j--)
+	for (currentRow, currentColumn; currentRow <= size - 1 && currentColumn >= 0; currentRow++, currentColumn--)
 	{
-		matrix[i][j] = currentNumber;
+		matrix[currentRow][currentColumn] = currentNumber;
 		currentNumber -= 1;
 	}
-	i--;
-	j++;
+	currentRow--;
+	currentColumn++;
 }
 
 // Function which outputs a matrix
@@ -111,4 +113,9 @@ void OutputMatrix(int** matrix, int rowNumber, int columnNumber)
 		}
 		cout << "\n";
 	}
+}
+
+void OutputCorners(int** matrix, int matrixSize)
+{
+	cout << matrix[0][0] << " " << matrix[matrixSize - 1][0] << " " << matrix[0][matrixSize - 1] << " " << matrix[matrixSize - 1][matrixSize - 1];
 }
