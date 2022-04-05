4b8bf9141d24b1d8da7b31724a6f47fd5e18814d
YuraRiabov <uraryabov@gmail.com>
2022-04-05 10:12:08 +0300

    Train class created

---
+++ 2 term/PB/PB Lab2.3/Train.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB_Lab2._3
+{
+    internal class Train
+    {
+    }
+}
