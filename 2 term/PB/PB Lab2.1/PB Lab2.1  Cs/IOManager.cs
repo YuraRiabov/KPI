461d15f90ed04835d7f42623ee3bef7f0faeb5c4
YuraRiabov <uraryabov@gmail.com>
2022-04-05 16:06:24 +0300

    GetText method added

---
+++ 2 term/PB/PB Lab2.1/PB Lab2.1  Cs/IOManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB_Lab2._1__Cs
+{
+    public class IOManager
+    {
+        public List<string> GetText()
+        {
+            List<string> text = new List<string>();
+            string currentString = "";
+            ConsoleKeyInfo currentKey = Console.ReadKey();
+            while (currentKey.Equals(ConsoleKey.E) == false || currentKey.Modifiers.Equals(ConsoleModifiers.Shift) == false)
+            {
+                if (currentKey.Equals(ConsoleKey.Enter))
+                {
+                    text.Add(currentString);
+                    currentString = "";
+                    Console.CursorTop++;
+                }
+                else if (currentKey.Equals(ConsoleKey.Backspace))
+                {
+                    currentString = currentString.Remove(currentString.Length - 1);
+                    Console.CursorLeft--;
+                }
+                else
+                {
+                    currentString += currentKey.KeyChar;
+                }
+                currentKey = Console.ReadKey();
+            }
+            return text;
+        }
+    }
+}
