89c6e28ce2ac0f870a9c1fb9bee0dc045592bc5a
YuraRiabov <uraryabov@gmail.com>
2022-04-05 10:36:24 +0300

    Train class fields and properties added

--- 2 term/PB/PB Lab2.3/Train.cs
+++ 2 term/PB/PB Lab2.3/Train.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace PB_Lab2._3
 {
-    internal class Train
+    public class Train
     {
+        private TimeOnly departureTime;
+        public int Number { get; set; }
+        public string Destination { get; set; }
+        public string DepartureTime
+        {
+            get
+            {
+                return $"{departureTime.Hour}-{departureTime.Minute}";
+            }
+            set
+            {
+                if (value[3] == '-' && int.TryParse(value.Substring(0, 2), out int hours) && int.TryParse(value.Substring(3, 2), out int minutes))
+                {
+                    departureTime = new TimeOnly(hours, minutes); 
+                }
+                else
+                {
+                    throw new ArgumentException("Wrong format of time");
+                }
+            }
+        }
+        private Train(int number, string destination)
+        {
+            this.Number = number;
+            this.Destination = destination;
+        }
+        public Train(int number, string destination, TimeOnly departureTime): this(number, destination)
+        {
+            this.departureTime = departureTime;
+        }
+        public Train(int number, string destination, string departureTime) : this(number, destination)
+        {
+            this.DepartureTime = departureTime;
+        }
     }
 }
