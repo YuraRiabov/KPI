d09257c7e3fafad1ec40df5e9ef8f190284fcf67
YuraRiabov <uraryabov@gmail.com>
2022-04-06 18:28:39 +0300

    IOMangager updated, some minor methods added

--- 2 term/PB/PB Lab2.1/PB Lab2.1  Cs/IOManager.cs
+++ 2 term/PB/PB Lab2.1/PB Lab2.1  Cs/IOManager.cs
@@ -44,5 +44,32 @@ namespace PB_Lab2._1__Cs
             }
             return text;
         }
+
+        public void InformUser()
+        {
+            Console.WriteLine(@"Enter your text, to stop entering press Ctrl+E");
+        }
+        public int AskLastLinesNumber()
+        {
+            Console.WriteLine("Enter number of last lines to transfer: ");
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Invalid input, try again");
+            }
+            return number;
+        }
+        public bool AskAppending()
+        {
+            Console.WriteLine("If you want to append file, if one exists, press 1, else press Enter");
+            if (Console.ReadLine() == "1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
