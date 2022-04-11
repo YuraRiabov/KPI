5cba5394663b08ec55ddb61c67719f0db9f5b39d
YuraRiabov <uraryabov@gmail.com>
2022-04-11 21:29:53 +0300

    Derived Prism classes implemented

---
+++ 2 term/PB/PB Lab2.5/PB Lab2.5 cs/Prism3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB_Lab2._5_cs
+{
+    public class Prism3 : Prism
+    {
+        public Prism3(double height, double sideLength) : base(height, sideLength)
+        {}
+
+        public override double CalculateBaseSurface()
+        {
+            return SideLength * SideLength * Math.Sqrt(3) / 4;
+        }
+
+        public override double CalculateSurface()
+        {
+            return CalculateBaseSurface() * 2 + SideLength * Height * 3;
+        }
+
+        public override double CalculateVolume()
+        {
+            return CalculateBaseSurface() * Height;
+        }
+    }
+}
---
+++ 2 term/PB/PB Lab2.5/PB Lab2.5 cs/Prism4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB_Lab2._5_cs
+{
+    internal class Prism4 : Prism
+    {
+        public Prism4(double height, double sideLength) : base(height, sideLength)
+        {}
+
+        public override double CalculateBaseSurface()
+        {
+            return SideLength * SideLength;
+        }
+
+        public override double CalculateSurface()
+        {
+            return CalculateBaseSurface() * 2 + SideLength * Height * 4;
+        }
+
+        public override double CalculateVolume()
+        {
+            return CalculateBaseSurface() * Height;
+        }
+    }
+}
