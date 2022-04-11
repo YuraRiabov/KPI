841e112413a87c5b264237f65f25434cc3b2ea45
YuraRiabov <uraryabov@gmail.com>
2022-04-11 22:34:13 +0300

    lab 2.5 finished cs

---
+++ 2 term/PB/PB Lab2.5/PB Lab2.5 cs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB_Lab2._5_cs
+{
+    public class Program
+    {
+        static void Main()
+        {
+            IOManager iOManager = new IOManager();
+            List<Prism> prisms = iOManager.GetPrisms();
+            iOManager.GetVolumeSurfaceData(prisms);
+        }
+    }
+}
