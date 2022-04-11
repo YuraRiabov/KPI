758da54bfc91d06a6011d0ddb1aaf9df3d088ccd
YuraRiabov <uraryabov@gmail.com>
2022-04-11 09:45:46 +0300

    Initial lab 2.2 commit, Client struct created

---
+++ 2 term/PB/PB Lab2.2/PB Lab2.2 cs/PB Lab2.2 cs/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB_Lab2._2_cs
+{
+    public struct Client
+    {
+        public string Name { get; set; }
+        public TimeOnly ComingTime { get; set; }
+        public TimeOnly LeavingTime { get; set; }
+    }
+}
