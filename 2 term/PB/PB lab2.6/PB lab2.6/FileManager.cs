4375cbce6c64b4741fca77869c010e61afb42789
YuraRiabov <uraryabov@gmail.com>
2022-05-11 10:59:22 +0300

    Class to work with files added

---
+++ 2 term/PB/PB lab2.6/PB lab2.6/FileManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB_lab2._6
+{
+    public class FileManager
+    {
+        public static List<string> GetWordsFromFile(string fileName)
+        {
+            string text;
+            List<string> words = new List<string>();
+            if (File.Exists(fileName))
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    text = sr.ReadToEnd();
+                }
+                words = text.Split(' ', ',', '.', '!', '?', '\n', '\r').Where(x => x.Length > 0).ToList();
+            }
+            return words;
+        }
+    }
+}
